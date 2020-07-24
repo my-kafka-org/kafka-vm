d745af35a468a312d7dfc36783928f564cc85e14
Thiru <thiru@ntucenterprise.sg>
2020-07-24 16:44:42 +0800

    Pull properties files from Google storage

--- boot.sh
+++ boot.sh
@@ -17,8 +17,12 @@ sudo wget https://apachemirror.sg.wuchna.com/kafka/2.5.0/kafka_2.13-2.5.0.tgz
 sudo gunzip kafka_2.13-2.5.0.tgz
 sudo tar -xvf kafka_2.13-2.5.0.tar
 sudo mkdir -p  /kafka_2.13-2.5.0/data/zookeeper
+sudo mkdir -p  /kafka_2.13-2.5.0/data/kafka
 sudo echo "export PATH=/kafka_2.13-2.5.0/bin:\$PATH" >> ~/.bashrc
+sudo gsutil cp gs://kafka-config-san/server.properties /kafka_2.13-2.5.0/config
+sudo gsutil cp gs://kafka-config-san/zookeeper.properties /kafka_2.13-2.5.0/config
+sudo zookeeper-server-start.sh /kafka_2.13-2.5.0/config/zookeeper.properties &
+sudo kafka-server-start.sh /kafka_2.13-2.5.0/config/server.properties &
 }
-
 install_nginx
 install_kafka
